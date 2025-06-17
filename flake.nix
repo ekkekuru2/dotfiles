442e8bb53ce35f80cdacc7c554b7d6df93ac9a07
ekkekuru2 <ekke@ekke.jp>
2025-06-17 14:06:16 +0900

    install winapps

--- flake.nix
+++ flake.nix
@@ -7,9 +7,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@ inputs:
+  outputs = { self, nixpkgs, home-manager, winapps, ... }@ inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -17,7 +22,31 @@
   in {
     nixosConfigurations.lemp13 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./nix/nixos/configuration.nix];
+      specialArgs = {
+        inherit inputs system;
+      };
+      modules = [ ./nix/nixos/configuration.nix
+        (
+            {
+              pkgs,
+              system ? pkgs.system,
+              ...
+            }:
+            {
+              # set up binary cache (optional)
+              nix.settings = {
+                substituters = [ "https://winapps.cachix.org/" ];
+                trusted-public-keys = [ "winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g=" ];
+              };
+
+              environment.systemPackages = [
+                winapps.packages."${system}".winapps
+                winapps.packages."${system}".winapps-launcher # optional
+              ];
+            }
+          )
+
+      ];
     };
     homeConfigurations."ekkekuru2" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;

aa8f2c20ea0033508690a2dfad193f65548d7f46
ekkekuru2 <ekke@ekke.jp>
2025-06-23 00:21:09 +0900

    nixos/configuration.nixでzsh.enable=trueにしているので、home-managerでは設定しない
    home.fileでファイルを配置したいのでhome-managerで設定すると二重になってしまう

--- nix/home-manager/home-manager.nix
+++ nix/home-manager/home-manager.nix
@@ -122,15 +122,6 @@
     };
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    history.size = 10000;
-  };
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;

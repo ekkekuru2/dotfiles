29a5c18fba146ee55981f6744b5ae2515315a897
ekkekuru2 <ekke@ekke.jp>
2025-06-29 14:15:08 +0900

    バッテリー接続中でもパフォーマンスは妥協しない

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -197,7 +197,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 100;
 
        #Optional helps save long term battery health
        START_CHARGE_THRESH_BAT0 = 0; # 電源に接続したらいつでも充電してほしい

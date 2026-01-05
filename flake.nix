df9358cd63af030aaca16d0481b6977c5e009c0f
ekkekuru2 <ekke@ekke.jp>
2026-01-05 09:23:07 +0900

    WinAppsのbinary cacheは使わない

--- flake.nix
+++ flake.nix
@@ -34,12 +34,6 @@
               ...
             }:
             {
-              # set up binary cache (optional)
-              nix.settings = {
-                substituters = [ "https://winapps.cachix.org/" ];
-                trusted-public-keys = [ "winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g=" ];
-              };
-
               environment.systemPackages = [
                 winapps.packages."${system}".winapps
                 winapps.packages."${system}".winapps-launcher # optional

dcd2aa61f2f03418367ad50b8f8ecdf29dd6b435
ekkekuru2 <ekke@ekke.jp>
2026-06-27 17:55:45 +0900

    change deprecated expression

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -64,8 +64,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # 20250505 Hyprland
   #programs.hyprland = {
@@ -86,7 +86,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
